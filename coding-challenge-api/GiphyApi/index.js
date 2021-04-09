31539f2b350af7297dee6f802c20e7ee3906fd9e
Matt Pileggi <matt@withjam.com>
2021-04-09 00:25:17 -0400

    simple code to call giphy random api with our api key stored in the server

--- coding-challenge-api/GiphyApi/index.js
+++ coding-challenge-api/GiphyApi/index.js
@@ -1,13 +1,36 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const bent = require('bent');
+const getJSON = bent('json');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+function getRandom(rating) {
+  return getJSON(
+    `${process.env.GIPHY_API}/random?api_key=${process.env.GIPHY_API_KEY}${
+      rating ? '&rating=' + rating : ''
+    }`
+  ).then((res) => res.data);
 }
+
+module.exports = async function (context, req) {
+  // let them specify an optional rating
+  const rating = req.query.rating || (req.body && req.body.rating);
+  // let them specify an optional count, default is 3
+  const length = req.query.count || (req.body && req.body.count) || 3;
+  // request them at the same time but only respond when they all have completed
+  const promises = Array.from({ length }, () => getRandom(rating));
+
+  context.log(
+    `JavaScript HTTP trigger function processed a request. ${
+      process.env.GIPHY_API
+    }/random?api_key=${process.env.GIPHY_API_KEY}${
+      rating ? '&rating=' + rating : ''
+    }`
+  );
+
+  const giphs = await Promise.all(promises);
+
+  context.res = {
+    body: {
+      total: giphs.length,
+      giphs,
+    },
+  };
+};
